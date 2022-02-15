fda97835c1cfa7ea425289c102ecb575944024cb
Khairo <khairo.khatib@gmail.com>
2022-02-14 23:11:31 -0500

    adds serving static files and refactored font files
    20 finalized the login page

---
+++ components/SideSection.js
@@ -0,0 +1,66 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+  Heading,
+  Text,
+  SimpleGrid,
+  GridItem,
+  Button,
+  useBreakpointValue,
+  HStack,
+  Link,
+  createStandaloneToast,
+  FormErrorMessage,
+  Box,
+  Flex,
+} from "@chakra-ui/react";
+import { RightArrow } from "./CustomIcons";
+import styles from "../styles/SideSection.module.css";
+
+const SideSection = () => {
+  return (
+    <VStack h="100%" w="full" py={20} px={10} className={styles.sideStack}>
+      <Flex
+        flexDirection="column"
+        justifyContent="space-between"
+        alignItems="center"
+        w="100%"
+        h="100%"
+      >
+        <Box m={0} p={0} h="0vh" w="0vw"></Box>
+
+        <Box justifySelf="end">
+          <Heading
+            as="h1"
+            size="4xl"
+            fontFamily="opensans-extrabold"
+            color="#fff"
+            
+          >
+            The
+            <br /> Anti-Covid
+            <br /> Web app
+          </Heading>
+          <Button
+            variant="ghost"
+            _hover={{ textDecoration: "underline" }}
+            size="lg"
+            type="submit"
+            color="white"
+          >
+            <Box fontSize={25}>QR-code</Box>
+            <RightArrow className={styles.qrBtnIcon} />
+          </Button>
+        </Box>
+
+        <Text color="#fff" alignSelf="start" justifySelf="flex-end">
+          www.anti-covid.com
+        </Text>
+      </Flex>
+    </VStack>
+  );
+};
+
+export default SideSection;
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,21 +1,42 @@
-import { Container, VStack, Flex } from "@chakra-ui/react";
+import { Container, VStack, Flex, Box } from "@chakra-ui/react";
 import LoginForm from "../components/LoginForm";
+import SideSection from "../components/SideSection";
 
-function LoginPage() {
+const LoginPage = () => {
   return (
     <Container
       maxW="full"
       p={0}
       bgGradient="linear(to-br, blue.600, purple.700)"
+      backgroundImage="url('/images/Background_Light.png')"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
     >
-      <Flex h="100vh" py={25} w="full">
-        <VStack bg="blue.50" w="full" h="full" alignItems="flex-start" />
-        <VStack w="full" h="full" p={0} justifyContent="center">
+      <Flex h="100vh"  w="100vw">
+        <VStack
+          display={{ base: "none", lg: "block" }}
+          w={{
+            lg: "45%",
+          }}
+          h="full"
+        >
+          <SideSection />
+        </VStack>
+        <VStack
+          w={{
+            base: "100%", // 0-48em
+            lg: "55%",
+          }}
+          h="full"
+          p={0}
+          justifyContent="center"
+          alignItems="center"
+        >
           <LoginForm />
         </VStack>
       </Flex>
     </Container>
   );
-}
+};
 
 export default LoginPage;
