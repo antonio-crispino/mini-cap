a884ff30376f247371d124bc0146811045d0537a
antonio-crispino <77855308+antonio-crispino@users.noreply.github.com>
2022-03-14 10:49:53 -0400

    Create SinglePatientStatus component

---
+++ components/medicaldoctor/SinglePatientStatus.jsx
@@ -0,0 +1,54 @@
+import { Box, List, ListItem } from "@chakra-ui/react";
+
+function SinglePatientStatus({ patientDetails, patientStatusDetails }) {
+  if (patientStatusDetails === null) {
+    return null;
+  }
+
+  let patientListItems = null;
+  let patientStatus = null;
+
+  if (patientDetails !== null) {
+    patientListItems = Object.keys(patientDetails).map((detail) => (
+      // check for null !!!
+      <ListItem>
+        {detail}: {patientDetails[detail]}
+      </ListItem>
+    ));
+    patientStatus =
+      patientDetails !== null &&
+      patientDetails.status !== null &&
+      patientDetails.status !== "none"
+        ? `(${patientDetails.status})`
+        : "";
+  }
+
+  const statusDate = patientStatusDetails.created_at;
+  const statusListItems = Object.keys(patientStatusDetails).map((detail) => (
+    // check for null !!!
+    <ListItem>
+      {detail}: {patientStatusDetails[detail]}
+    </ListItem>
+  ));
+
+  return (
+    <Box>
+      {patientListItems !== null ? (
+        <Box>
+          <Box>Patient {patientStatus}</Box>
+          <List>{patientListItems}</List>
+        </Box>
+      ) : null}
+      <Box>
+        <Box>Date: {statusDate.substring(0, 10)}</Box>
+        <Box>Time: {statusDate.substring(11, 19)}</Box>
+      </Box>
+      <Box>
+        <Box>Latest Status</Box>
+        <List>{statusListItems}</List>
+      </Box>
+    </Box>
+  );
+}
+
+export default SinglePatientStatus;
