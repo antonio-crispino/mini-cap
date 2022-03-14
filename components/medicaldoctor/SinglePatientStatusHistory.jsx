a60a87b1836e4760bba9dbc12127e90e319732ff
antonio-crispino <77855308+antonio-crispino@users.noreply.github.com>
2022-03-14 12:15:36 -0400

    Create SinglePatientStatusHistory component

---
+++ components/medicaldoctor/SinglePatientStatusHistory.jsx
@@ -0,0 +1,43 @@
+import { Box, List, ListItem } from "@chakra-ui/react";
+import SinglePatientStatus from "./SinglePatientStatus";
+
+function SinglePatientStatusHistory({ patientDetails, allPatientStatuses }) {
+  if (patientDetails === null) {
+    return null;
+  }
+
+  const patientListItems = Object.keys(patientDetails).map((detail) => (
+    // check for null !!!
+    <ListItem>
+      {detail}: {patientDetails[detail]}
+    </ListItem>
+  ));
+
+  const patientStatuses = null;
+
+  if (allPatientStatuses !== null) {
+    Object.keys(allPatientStatuses).map((status) => (
+      <SinglePatientStatus
+        patientDetails={null}
+        patientStatusDetails={status}
+      />
+    ));
+  }
+
+  return (
+    <Box>
+      <Box>
+        <Box>
+          Patient
+          {patientDetails.status !== null && patientDetails.status !== "none"
+            ? ` (${patientDetails.status})`
+            : ""}
+        </Box>
+        <List>{patientListItems}</List>
+      </Box>
+      <Box>{patientStatuses}</Box>
+    </Box>
+  );
+}
+
+export default SinglePatientStatusHistory;
