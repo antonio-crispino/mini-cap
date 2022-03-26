313bf82b81988317d313bc8c8ba6961aa310ca99
denzz312 <denzz312312@gmail.com>
2022-03-25 23:57:58 -0400

    displaying qrcode based on user's id

--- components/qrcode/QRCodeSection.jsx
+++ components/qrcode/QRCodeSection.jsx
@@ -1,15 +1,26 @@
 import { VStack, Text } from "@chakra-ui/react";
+import QRCode from "qrcode.react";
 import { useAppContext } from "../../context/AppContext";
 
 function QRCodeSection() {
   const { user } = useAppContext();
+
+  const generateQR = (
+    <QRCode
+      size={300}
+      value={user.id}
+      bgColor="white"
+      fgColor="black"
+      level="H"
+    />
+  );
 
   return (
     <VStack>
       <Text fontSize="5xl" fontFamily="opensans-extrabold" color="#fff">
         {user?.firstname} {user?.lastname}
       </Text>
-      <Text>Here will be a qrcode</Text>
+      {generateQR}
     </VStack>
   );
 }
