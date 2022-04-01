14832870d184752aee1e647bfe3be8c06631de70
Khairo <khairo.khatib@gmail.com>
2022-04-01 02:27:39 -0400

    added form validation for requesting appointmenet #105

--- components/RequestPatientAppointment.jsx
+++ components/RequestPatientAppointment.jsx
@@ -25,6 +25,12 @@ import { useAppContext } from "../context/AppContext";
 function RequestPatientAppointment({ patientData }) {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [isError, setIsError] = useState(false);
+  const [emptyFields, setEmptyFields] = useState({
+    location: false,
+    subject: false,
+    date: false,
+    time: false,
+  });
 
   const [location, setLocation] = useState("");
   const [notes, setNotes] = useState("");
@@ -65,6 +71,7 @@ function RequestPatientAppointment({ patientData }) {
   const subjectChangeHandler = (e) => {
     const inputValue = e.target.value;
     setSubject(inputValue);
+    setEmptyFields({ ...emptyFields, subject: inputValue === "" });
   };
 
   const notesChangeHandler = (e) => {
@@ -75,12 +82,14 @@ function RequestPatientAppointment({ patientData }) {
   const locationChangeHandler = (e) => {
     const inputValue = e.target.value;
     setLocation(inputValue);
+    setEmptyFields({ ...emptyFields, location: inputValue === "" });
   };
 
   const timeChangeHandler = (e) => {
     const time = e.target.value;
     const isValid = time.match("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$");
     setIsError(isValid === null);
+    setEmptyFields({ ...emptyFields, time: time === "" });
     if (isValid) {
       const timeArray = time.split(":");
       selectedDate.setHours(timeArray[0], timeArray[1]);
@@ -100,17 +109,18 @@ function RequestPatientAppointment({ patientData }) {
           <ModalHeader>Specify Appointment Details</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <FormControl my={5}>
+            <FormControl my={5} isInvalid={emptyFields.subject}>
               <FormLabel>Subject</FormLabel>
               <Input
                 id="subject"
                 placeholder="Appointment title here ..."
                 onChange={(e) => subjectChangeHandler(e)}
+                onFocus={(e) => subjectChangeHandler(e)}
                 bg="white"
                 size="lg"
               />
             </FormControl>
-            <FormControl my={5}>
+            <FormControl my={5} isInvalid={emptyFields.date}>
               <FormLabel htmlFor="date-input" placeholder="date">
                 Date
               </FormLabel>
@@ -138,12 +148,13 @@ function RequestPatientAppointment({ patientData }) {
                 }}
               />
             </FormControl>
-            <FormControl isInvalid={isError} my={5}>
+            <FormControl isInvalid={isError || emptyFields.time} my={5}>
               <FormLabel htmlFor="time">Time</FormLabel>
               <Input
                 id="time"
                 placeholder="Format must be HH:MM"
                 onChange={(e) => timeChangeHandler(e)}
+                onFocus={(e) => timeChangeHandler(e)}
                 bg="white"
                 size="lg"
               />
@@ -156,13 +167,14 @@ function RequestPatientAppointment({ patientData }) {
               )}
             </FormControl>
 
-            <FormControl my={5}>
+            <FormControl my={5} isInvalid={emptyFields.location}>
               <FormLabel htmlFor="location">Location</FormLabel>
 
               <Input
                 id="location"
                 value={location}
                 onChange={(e) => locationChangeHandler(e)}
+                onFocus={(e) => locationChangeHandler(e)}
                 placeholder="Here is a sample placeholder"
                 size="lg"
               />
@@ -189,7 +201,18 @@ function RequestPatientAppointment({ patientData }) {
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" onClick={submitAppointment}>
+            <Button
+              colorScheme="blue"
+              onClick={submitAppointment}
+              disabled={
+                emptyFields.date ||
+                emptyFields.location ||
+                emptyFields.subject ||
+                emptyFields.time ||
+                subject === "" ||
+                isError
+              }
+            >
               Schedule Appointment
             </Button>
             <Button colorScheme="red" ml={3} onClick={onClose}>
